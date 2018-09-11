911c2b52294d97edeb6eef49f95f6205c915d142
user <user@192.168.219.183>
2018-09-11 19:48:11 +0900

    test

---
+++ src/main/java/skhu/SkhuDbProjectApplication.java
@@ -0,0 +1,12 @@
+package skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhuDbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkhuDbProjectApplication.class, args);
+	}
+}
