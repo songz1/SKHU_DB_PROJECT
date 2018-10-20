60768a14abb6bdfd8ae649043650ac839f567102
user <user@192.168.219.104>
2018-10-20 17:56:34 +0900

    10.20 17:56 controller admin, user 분리

--- src/main/java/skhu/controller/admin/LoginController.java
+++ src/main/java/skhu/controller/admin/LoginController.java
@@ -1,15 +1,15 @@
-package skhu.controller;
+package skhu.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("user/login")
+@RequestMapping("admin/login")
 public class LoginController {
 
 	@RequestMapping(value="login", method=RequestMethod.GET)
 	public String login() {
-		return "user/login/login";
+		return "admin/login/login";
 	}
 }
