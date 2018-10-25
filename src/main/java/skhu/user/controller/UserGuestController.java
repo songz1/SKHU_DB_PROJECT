895c4db77fb328d2b11b42560a39079359239f7a
songz1 <21pori30@gmail.com>
2018-10-26 03:06:18 +0900

    test

--- src/main/java/skhu/user/controller/UserGuestController.java
+++ src/main/java/skhu/user/controller/UserGuestController.java
@@ -8,24 +8,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("user/login/guest")
 public class UserGuestController {
 
-	@RequestMapping(value = "basic", method = RequestMethod.GET)
-	public String basic() {
-		return "user/login/guest/basic";
+	@RequestMapping(value = "guestBasic", method = RequestMethod.GET)
+	public String guestBasic() {
+		return "user/login/guest/guestBasic";
 	}
 
-	@RequestMapping(value = "detail", method = RequestMethod.GET)
-	public String detail() {
-		return "user/login/guest/detail";
+	@RequestMapping(value = "guestDetail", method = RequestMethod.GET)
+	public String guestDetail() {
+		return "user/login/guest/guestDetail";
 	}
 
-	@RequestMapping(value = "yearChoice", method = RequestMethod.GET)
-	public String yearChoice() {
-		return "user/login/guest/yearChoice";
+	@RequestMapping(value = "guestYearChoice", method = RequestMethod.GET)
+	public String guestYearChoice() {
+		return "user/login/guest/guestYearChoice";
 	}
 
-	@RequestMapping(value = "info", method = RequestMethod.GET)
-	public String info() {
-		return "user/login/guest/info";
+	@RequestMapping(value = "guestInfo", method = RequestMethod.GET)
+	public String guestInfo() {
+		return "user/login/guest/guestInfo";
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +7,8 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>mybatis</h1>
-<a href="user/login/login">login</a>
-<br />
+	<h1>mybatis</h1>
+	<a href="user/login/login">login</a>
+	<br />
 </body>
 </html>
