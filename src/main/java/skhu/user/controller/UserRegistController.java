27dc364af00c8d4d962a3aa9365574a066d7a078
jieun <21pori30@gmail.com>
2018-10-28 23:43:33 +0900

    아 모르게쒀여

--- src/main/java/skhu/user/controller/UserRegistController.java
+++ src/main/java/skhu/user/controller/UserRegistController.java
@@ -1,15 +1,33 @@
 package skhu.user.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import skhu.dto.Department;
+import skhu.dto.Student;
+import skhu.mapper.DepartmentMapper;
+import skhu.mapper.StudentMapper;
 
 @Controller
 @RequestMapping("user/regist")
 public class UserRegistController {
+	@Autowired StudentMapper studentMapper;
+	@Autowired DepartmentMapper departmentMapper;
 
 	@RequestMapping(value = "regist", method = RequestMethod.GET)
-	public String regist() {
+	public String regist(Model model) {
+		Student student = new Student();
+		List<Department> departments = departmentMapper.findAll();
+
+		model.addAttribute("student", student);
+		model.addAttribute("departments", departments);
+		
 		return "user/regist/regist";
 	}
 
