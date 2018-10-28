b4218ad7432f10eceff63d8f27515728396f423f
user <user@192.168.219.104>
2018-10-29 00:02:48 +0900

    test

--- src/main/java/skhu/mapper/StudentMapper.java
+++ src/main/java/skhu/mapper/StudentMapper.java
@@ -3,13 +3,14 @@ package skhu.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import skhu.dto.Student;
 
 @Mapper
 public interface StudentMapper {
 	List<Student> findAll();
-	Student login(String studentNumber, String password);
+	Student login(@Param("studentNumber")String studentNumber, @Param("password")String password);
 	Student findById(int id);
 	void update(Student student);
 	void insert(Student student);
